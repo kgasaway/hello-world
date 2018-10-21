16602c47d71b01157580a363bf0862ec4b7b52e3
Kacie Gasaway <kacie.gasaway@iqcu.com>
2018-10-20 23:41:36 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,21 @@
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
+    /**
+     * Say Hello
+     *
+     * @author Kacie Gasaway
+     * @version 10.20.18
+     */
+
+    /**
+     * Say hello to the world
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+
 }
+
+
